db24dd6d31a20215069aba5fe503d078b3e5772b
Shuichi Saito <s-saito@jinso.co.jp>
2026-02-16 13:15:22 +0900

    init spring boot

---
+++ microwave-reservation/src/main/java/com/microwave/reservation/MicrowaveReservationApplication.java
@@ -0,0 +1,13 @@
+package com.microwave.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrowaveReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrowaveReservationApplication.class, args);
+	}
+
+}
---
+++ microwave-reservation/src/test/java/com/microwave/reservation/MicrowaveReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microwave.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrowaveReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
